7621de97747eda55bf638c33e1758626d14a6fb2
aintira11 <aintrlala@gmail.com>
2025-10-06 22:10:13 +0700

    gamei

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-// import { router as index } from "./api/index"; 
+import { router as index } from "./api/index"; 
 import { router as user } from "./api/user"; 
 
 export const app = express();
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", user);
+app.use("/game", index);
 
 // app.use("/", (req, res) => {
 //   res.send("Hello World!!!");
